e83bd28c48fa4bfca1f62ea785eb65d99b15d06b
SommerNicolas <nicolas.sommer@he-arc.ch>
2017-02-20 13:31:19 +0100

    login with password clear

--- code/app/controllers/application_controller.rb
+++ code/app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authorize
+  protected
+  def authorize
+    unless User.find_by(id: session[:user_id])
+      redirect_to login_url, notice:"NOPE !!!"
+    end
+  end
 end
