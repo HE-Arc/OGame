97da992432874cfa6f17c9c6e8c2554b8ffc0366
cyrilruedin <cyril.ruedin@he-arc.ch>
2017-03-22 15:58:13 +0100

    distances

--- code/app/controllers/planet_controller.rb
+++ code/app/controllers/planet_controller.rb
@@ -5,9 +5,7 @@ class PlanetController < ApplicationController
 
   def show
     @selected_planet = Planet.find params[:id]
-    @solar_system = Solarsystem.find(@selected_planet)
-    @range = Math.sqrt((Planet.find(@actual_user.planet_id).position_x-@selected_planet.position_x)**2 +
-                       (Planet.find(@actual_user.planet_id).position_y-@selected_planet.position_y)**2 +
-                       (Planet.find(@actual_user.planet_id).position_z-@selected_planet.position_z)**2)
+    #@solar_system = Solarsystem.find(@selected_planet)
+    @range = Planet.find(@actual_user.planet_id).distanceto(@selected_planet)
   end
 end
