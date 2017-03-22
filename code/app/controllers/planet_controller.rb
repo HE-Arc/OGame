11f102533045d683067906fe61a41d6f1030e6c4
cyrilruedin <cyril.ruedin@he-arc.ch>
2017-03-22 15:10:40 +0100

    calcul de distance planete

--- code/app/controllers/planet_controller.rb
+++ code/app/controllers/planet_controller.rb
@@ -5,5 +5,9 @@ class PlanetController < ApplicationController
 
   def show
     @selected_planet = Planet.find params[:id]
+    @solar_system = Solarsystem.find(@selected_planet)
+    @range = Math.sqrt((Planet.find(@actual_user.planet_id).position_x-@selected_planet.position_x)**2 +
+                       (Planet.find(@actual_user.planet_id).position_y-@selected_planet.position_y)**2 +
+                       (Planet.find(@actual_user.planet_id).position_z-@selected_planet.position_z)**2)
   end
 end
