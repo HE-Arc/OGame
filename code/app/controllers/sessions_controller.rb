2e051d36a2318a05edf99a5e635356ac355c9fae
SommerNicolas <sommer.nicolas92@gmail.com>
2017-03-29 12:28:51 +0200

    fixed link after connexion

--- code/app/controllers/sessions_controller.rb
+++ code/app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(name: params[:name])
     if user and user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to admin_url
+      redirect_to user_path(user.id) #=> change redirect to home
     else
       redirect_to login_url, alert:"Invalid Username or Password"
     end
