fdde9cb1cc40d1a295cb2204ec636340872b7e8a
cyrilruedin <cyril.ruedin@he-arc.ch>
2017-03-29 15:46:30 +0200

    page planète affichage slot et batiment

--- code/app/controllers/planet_controller.rb
+++ code/app/controllers/planet_controller.rb
@@ -7,5 +7,6 @@ class PlanetController < ApplicationController
     @selected_planet = Planet.find params[:id]
     #@solar_system = Solarsystem.find(@selected_planet)
     @range = Planet.find(@actual_user.planet_id).distanceto(@selected_planet)
+    @buildings = Building.all
   end
 end
