3ad9076a50dc8dbee15b28d75e25f87036426027
Guillaume Petitpierre <guillaume.petitpierre@bluewin.ch>
2017-04-14 10:35:39 +0200

    Update ressources
    Might be working, trying on deployment server, since cron jobs won't work in docker container...

--- code/app/models/user.rb
+++ code/app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
   has_one :planet
 
   has_and_belongs_to_many :technologies
+
+  def self.update_ressources()
+    User.all.each do |u|
+      u.buildings.each do |b|
+        u.metal += b.metalFarmPerMinute
+        u.energy += b.energyFarmPerMinute
+      end
+      u.save
+    end
+  end
 end
